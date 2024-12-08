79385e8b2c5f7d839536dda7db499532b1a929e9
Lucas Faria <lucas.faria@lumegrupo.com.br>
2024-12-08 16:36:11 -0300

    First Commit

---
+++ src/main/java/br/com/lucasfaria/sboot_face_comparator/SbootFaceComparatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasfaria.sboot_face_comparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootFaceComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootFaceComparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasfaria/sboot_face_comparator/SbootFaceComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasfaria.sboot_face_comparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootFaceComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
